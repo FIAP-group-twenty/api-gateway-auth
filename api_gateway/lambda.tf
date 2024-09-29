4f84a7836d49fcb80ea51540a0cdab697d18f6d7
CarolPera <carol_p.100@hotmail.com>
2024-09-29 17:32:12 -0300

    ajustes

--- api_gateway/lambda.tf
+++ api_gateway/lambda.tf
@@ -1,9 +1,9 @@
 resource "aws_lambda_function" "authorizer_lambda" {
-  function_name = "AuthorizerLambda"
-  handler       = "lambda_function.lambda_handler"
-  runtime       = "python3.9"
-  role          = aws_iam_role.lambda_exec_role.arn
-  filename = "${path.module}/../lambda/lambda_authorizer.zip"
+  function_name    = "AuthorizerLambda"
+  handler          = "lambda_function.lambda_handler"
+  runtime          = "python3.9"
+  role             = data.aws_iam_role.lambda_exec_role.arn
+  filename         = "${path.module}/../lambda/lambda_authorizer.zip"
   source_code_hash = filebase64sha256("${path.module}/../lambda/lambda_authorizer.zip")
 
   environment {
@@ -14,38 +14,26 @@ resource "aws_lambda_function" "authorizer_lambda" {
 }
 
 resource "aws_api_gateway_authorizer" "jwt_lambda" {
-  name                   = "LambdaAuthorizer"
-  rest_api_id            = aws_api_gateway_rest_api.tech_challenge.id
-  authorizer_uri         = "arn:aws:apigateway:${var.aws_region}:lambda:path/2015-03-31/functions/${aws_lambda_function.authorizer_lambda.arn}/invocations"
+  name                             = "LambdaAuthorizer"
+  rest_api_id                      = aws_api_gateway_rest_api.tech_challenge.id
+  authorizer_uri                   = "arn:aws:apigateway:${var.aws_region}:lambda:path/2015-03-31/functions/${aws_lambda_function.authorizer_lambda.arn}/invocations"
   authorizer_result_ttl_in_seconds = 300
-  type                   = "REQUEST"
+  type                             = "REQUEST"
 }
 
-resource "aws_iam_role" "lambda_exec_role" {
+data "aws_iam_role" "lambda_exec_role" {
   name = "LambdaExecRole"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Action = "sts:AssumeRole",
-        Effect = "Allow",
-        Principal = {
-          Service = "lambda.amazonaws.com"
-        }
-      }
-    ]
-  })
 }
 
 resource "aws_iam_role_policy" "lambda_policy" {
-  role = aws_iam_role.lambda_exec_role.id
+  role = data.aws_iam_role.lambda_exec_role.id
 
   policy = jsonencode({
-    Version = "2012-10-17",
+    Version   = "2012-10-17",
     Statement = [
       {
-        Effect   = "Allow",
-        Action   = [
+        Effect = "Allow",
+        Action = [
           "logs:CreateLogGroup",
           "logs:CreateLogStream",
           "logs:PutLogEvents"
@@ -53,15 +41,15 @@ resource "aws_iam_role_policy" "lambda_policy" {
         Resource = "arn:aws:logs:*:*:*"
       },
       {
-        Effect   = "Allow",
-        Action   = [
+        Effect = "Allow",
+        Action = [
           "lambda:InvokeFunction"
         ],
         Resource = aws_lambda_function.authorizer_lambda.arn
       },
       {
-        Effect   = "Allow",
-        Action   = [
+        Effect = "Allow",
+        Action = [
           "cognito-idp:*"
         ],
         Resource = aws_cognito_user_pool.user_pool.arn
@@ -80,5 +68,3 @@ resource "aws_lambda_permission" "allow_api_gateway" {
 
   source_arn = "arn:aws:execute-api:${var.aws_region}:${data.aws_caller_identity.current.account_id}:${aws_api_gateway_rest_api.tech_challenge.id}/*"
 }
-
-
